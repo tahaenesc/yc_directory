4c262e0122c758641e31730cb1805b69036ad22e
tahaenesc <141641915+tahaenesc@users.noreply.github.com>
2024-11-10 21:49:00 +0300

    new species

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,53 @@
-export default function Home() {
+import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}) {
+  const query = (await searchParams).query;
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: "Adrian" },
+      _id: 1,
+      description: "This is a description.",
+      image:
+        "https://images.indianexpress.com/2024/10/Tech-feature-images27.jpg",
+      category: "Robots",
+      title: "We Robots",
+    },
+  ];
   return (
     <>
-      <h1 className="heading">
-        Home
-      </h1>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br />
+          Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Idaes, Vote on Pitches, and Get Noticed in Virtual
+          Competitions.
+        </p>
+
+        <SearchForm query={query} />
+      </section>
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+          <ul className="mt-7 card_grid">
+            {posts.length > 0 ? (
+              posts.map((post: StartupCardType, index: number) => (
+                <StartupCard key={post?._id} post={post} />
+              ))
+            ) : (
+              <p className="no-results">No startups found</p>
+            )}
+          </ul>
+        </p>
+      </section>
     </>
   );
 }
